5e36ce56939a055a8908b80eeb0bd41ad720269e
kidat <toshiro.kida@gmail.com>
2015-04-10 14:44:01 -0400

    splitting out CTL Admin theme, finally got pattern library css to read

---
+++ lib/themes/ctl_admin/template.php
@@ -0,0 +1,20 @@
+<?php
+
+function ctl_admin_preprocess_html(&$vars) {
+    $adminimal_path = drupal_get_path('theme', 'ctl_admin');
+
+    drupal_add_css($adminimal_path . '/css/ctl.css',
+        array(
+            'group' => CSS_THEME,
+            'media' => 'all',
+            'weight' => 5
+        )
+    );
+
+    drupal_add_js($adminimal_path . '/scripts/scroll-left.js');
+
+}
+
+function ctl_theme_date_combo($variables) {
+    return theme('form_element', $variables);
+}
