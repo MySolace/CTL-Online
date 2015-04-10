1ffe5fad4c90d2098546d8ecfb0c008b98095415
kidat <toshiro.kida@gmail.com>
2015-04-10 18:07:02 -0400

    finally getting going. working on forms now

--- lib/themes/ctl_theme/template.php
+++ lib/themes/ctl_theme/template.php
@@ -27,3 +27,82 @@ function ctl_theme_preprocess_html(&$vars) {
 function ctl_theme_date_combo($variables) {
     return theme('form_element', $variables);
 }
+
+function ctl_theme_breadcrumb($variables) {
+  $breadcrumb = $variables ['breadcrumb'];
+
+  $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+  $output .= '<div class="breadcrumb">&nbsp;';
+
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output .= implode(' » ', $breadcrumb);
+  }
+
+  $output .= '</div>';
+  return $output;
+}
+
+function ctl_theme_form_element($variables) {
+  $element = &$variables ['element'];
+
+  // This function is invoked as theme wrapper, but the rendered form element
+  // may not necessarily have been processed by form_builder().
+  $element += array(
+    '#title_display' => 'before',
+  );
+
+  // Add element #id for #type 'item'.
+  if (isset($element ['#markup']) && !empty($element ['#id'])) {
+    $attributes ['id'] = $element ['#id'];
+  }
+  // Add element's #type and #name as class to aid with JS/CSS selectors.
+  $attributes ['class'] = array('form-item');
+  $attributes ['class'][] = 'form-group';
+  if (!empty($element ['#type'])) {
+    $attributes ['class'][] = 'form-type-' . strtr($element ['#type'], '_', '-');
+  }
+  if (!empty($element ['#name'])) {
+    $attributes ['class'][] = 'form-item-' . strtr($element ['#name'], array(' ' => '-', '_' => '-', '[' => '-', ']' => ''));
+  }
+  // Add a class for disabled elements to facilitate cross-browser styling.
+  if (!empty($element ['#attributes']['disabled'])) {
+    $attributes ['class'][] = 'form-disabled';
+  }
+  $output = '<div' . drupal_attributes($attributes) . '>' . "\n";
+
+  // If #title is not set, we don't display any label or required marker.
+  if (!isset($element ['#title'])) {
+    $element ['#title_display'] = 'none';
+  }
+  $prefix = isset($element ['#field_prefix']) ? '<span class="field-prefix">' . $element ['#field_prefix'] . '</span> ' : '';
+  $suffix = isset($element ['#field_suffix']) ? ' <span class="field-suffix">' . $element ['#field_suffix'] . '</span>' : '';
+
+  switch ($element ['#title_display']) {
+    case 'before':
+    case 'invisible':
+      $output .= ' ' . theme('form_element_label', $variables);
+      $output .= ' ' . $prefix . $element ['#children'] . $suffix . "\n";
+      break;
+
+    case 'after':
+      $output .= ' ' . $prefix . $element ['#children'] . $suffix;
+      $output .= ' ' . theme('form_element_label', $variables) . "\n";
+      break;
+
+    case 'none':
+    case 'attribute':
+      // Output no label and no required marker, only the children.
+      $output .= ' ' . $prefix . $element ['#children'] . $suffix . "\n";
+      break;
+  }
+
+  if (!empty($element ['#description'])) {
+    $output .= '<div class="description">' . $element ['#description'] . "</div>\n";
+  }
+
+  $output .= "</div>\n";
+
+  return $output;
+}
