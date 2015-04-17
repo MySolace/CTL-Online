d1573c06f6c07f984b1ad94eb92e6548862b95d4
Chris Johnson <chris@crisistextline.org>
2015-04-17 14:11:40 -0400

    Theme fixes

--- lib/themes/ctl_theme/template.php
+++ lib/themes/ctl_theme/template.php
@@ -11,7 +11,27 @@ function ctl_theme_preprocess_html(&$vars) {
     );
 
     //drupal_add_js($adminimal_path . '/scripts/scroll-left.js');
+    drupal_add_js($adminimal_path . '/js/label.js');
 }
+
 function ctl_theme_date_combo($variables) {
     return theme('form_element', $variables);
+}
+
+function ctl_theme_menu_local_tasks(&$variables) {
+    $output = '';
+
+    if (!empty($variables['primary'])) {
+        $variables['primary']['#prefix'] = '<h2 class="element-invisible">' . t('Primary tabs') . '</h2>';
+        $variables['primary']['#prefix'] .= '<ul class="nav nav-tabs">';
+        $variables['primary']['#suffix'] = '</ul>';
+        $output .= drupal_render($variables['primary']);
+    }
+
+    if (!empty($variables['secondary'])) {
+        $output .= drupal_render($varialbes['secondary']);
+
+    }
+
+    return $output;
 }
---
+++ lib/themes/ctl_theme/templates/page.tpl.php
@@ -0,0 +1,99 @@
+<?php
+/**
+* @file
+* Main page template.
+*/
+?>
+<!--id="branding"-->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="/">CTL ONLINE</a>
+    </div>
+</nav>
+
+<?php /*
+<div class="container">
+    <?php print $breadcrumb; ?>
+</div>
+*/ ?>
+
+<div id="navigation">
+    <div class="container">
+
+
+  <?php if ($secondary_local_tasks): ?>
+    <div class="tabs-secondary clearfix"><ul class="tabs secondary"><?php print render($secondary_local_tasks); ?></ul></div>
+  <?php endif; ?>
+</div>
+</div>
+
+<div id="page">
+    <div class="container">
+        <?php print render($title_prefix); ?>
+
+        <?php if ($title): ?>
+                <h1 class="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
+
+        <?php print render($title_suffix); ?>
+
+        <div class="collapse navbar-collapse">
+            <?php if ($primary_local_tasks): ?>
+                <?php print render($primary_local_tasks); ?>
+            <?php endif; ?>
+        </div>
+
+        <div id="content" class="clearfix">
+                <div class="element-invisible"><a id="main-content"></a></div>
+
+        <?php if ($messages): ?>
+                <div id="console" class="clearfix"><?php print $messages; ?></div>
+        <?php endif; ?>
+
+        <?php if ($page['help']): ?>
+                <div id="help">
+                        <?php print render($page['help']); ?>
+                </div>
+        <?php endif; ?>
+
+        <?php if (isset($page['content_before'])): ?>
+                <div id="content-before">
+                        <?php print render($page['content_before']); ?>
+                </div>
+        <?php endif; ?>
+
+        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+
+  <div id="content-wrapper">
+
+    <?php if (isset($page['sidebar_left'])): ?>
+      <div id="sidebar-left">
+        <?php print render($page['sidebar_left']); ?>
+      </div>
+    <?php endif; ?>
+
+    <div id="main-content">
+            <?php print render($page['content']); ?>
+          </div>
+
+    <?php if (isset($page['sidebar_right'])): ?>
+      <div id="sidebar-right">
+        <?php print render($page['sidebar_right']); ?>
+      </div>
+    <?php endif; ?>
+
+        </div>
+
+        <?php if (isset($page['content_after'])): ?>
+                <div id="content-after">
+                        <?php print render($page['content_after']); ?>
+                </div>
+        <?php endif; ?>
+
+        </div>
+
+        <div id="footer">
+                <?php print $feed_icons; ?>
+        </div>
+    </div>
+</div>
