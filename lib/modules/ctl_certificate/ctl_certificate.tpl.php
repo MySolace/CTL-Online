b16c6912126f1bf179925fb29e4fe200bee83ca3
Ron Martinez <ron@crisistextline.org>
2015-08-03 12:20:06 -0400

    Worked on the certificate and the email.

---
+++ lib/modules/ctl_certificate/ctl_certificate.tpl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file
+ * Example template file for TCPDF Example module.
+ */
+?>
+<html>
+<head>
+  <title>TCPDF for Drupal</title>
+</head>
+<body>
+<?php print $certificate_name; ?>
+<span><?php print $graduate_name; ?></span>
+<span><?php print $certification_date; ?></span>
+</body>
+</html>
