aab0f3faae87c15ccecb42e2f0bb2c436dd8361e
Ron Martinez <ron@crisistextline.org>
2015-09-15 19:51:42 -0400

    Minor code cleanup.  Updated the graduation email.

--- lib/modules/ctl_graduation/templates/graduation_message.tpl.php
+++ lib/modules/ctl_graduation/templates/graduation_message.tpl.php
@@ -1,21 +1,44 @@
 <p>Hi <strong><?php print $graduate_name; ?></strong>,</p>
+
 <p>Congratulations on graduating from the Crisis Counselor Training Program! I am so excited to welcome you to the amazing team of crisis counselors who help save lives every day.</p>
+
 <p><strong>You’re a newly minted crisis counselor, now what?</strong></p>
-<p><strong>First, sign up for your weekly counseling shift.</strong></p>
+
+<p><strong>First, celebrate your accomplishment.</strong></p>
+<ul>
+  <li>
+    Access your certificate of completion here: <a href="https://online.crisistextline.org/user/<?php print $graduate_uid; ?>/certificate">https://online.crisistextline.org/user/<?php print $graduate_uid; ?>/certificate</a>
+    <ul>
+      <li>Print it out and display it proudly!</li>
+    </ul>
+  </li>
+  <li>
+    Share the good news with friends and family and invite them to go through training so they can join you on the platform: crisistextline.org/volunteer. Volunteering is always more fun with a friend. Our next application deadline is <strong>[site:next_due_date]</strong>.
+    <ul>
+      <li><a href="https://twitter.com/intent/tweet?text=Woo%21%20You%27re%20looking%20at%20a%20newly-minted%20%40CrisisTextLine%20crisis%20counselor%21%20Come%20save%20lives%20with%20me%3A%20http%3A%2F%2Fcrisistextline.org%2Fvolunteer&source=webclient">Twitter</a></li>
+      <li><a href="https://www.facebook.com/login.php?next=https%3A%2F%2Fwww.facebook.com%2Fsharer%2Fsharer.php%3Fu%3Dcrisistextline.org%252Fvolunteer%26ret%3Dlogin&display=popup">Facebook</a></li>
+      <li><a href="https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2FpostLogin%3Fsession_rikey%3DwNPHRSO3dcxDUe0v-qmScfRF750Nbez-4y2rllZxykPg17oJcOC3qTWcU5-4nZOU5t_r50pFHsEFXvZln9cpQTyxU8lllA5W1m1%26l%3Dhttps%253A%252F%252Fwww%252Elinkedin%252Ecom%252FshareArticle%253Fmini%253Dtrue%2526source%253Dcrisistextline%25252Eorg%2526title%253DBecome%252Ba%252Bcrisis%252Bcounselor%2526url%253Dcrisistextline%25252Eorg%25252Fvolunteer%26id%3D0%26b%3D41b363a3-d7f1-47f5-831d-2087fe0f8e14%26h%3DwQ5H%26m%3DGET">LinkedIn</a></li>
+    </ul>
+  </li>
+</ul>
+
+<p><strong>Then, sign up for your weekly counseling shift.</strong></p>
 <ul>
   <li>Use the scheduling tool in CTL Online: <a href="https://online.crisistextline.org/schedule/">https://online.crisistextline.org/schedule/</a></li>
-  <li>Click "<strong>Sign up for Shift</strong>" &gt; choose a day of the week &gt; select an available shift (note: all times are in EST) &gt; confirm by hitting "<strong>Sign up for this shift</strong>" button at the bottom</li>
+  <li>Click “<strong>Sign up for Shift</strong>” &gt; choose a day of the week &gt; select an available shift (note: all times are in EST) &gt; confirm by hitting the “<strong>Sign up for this shift</strong>” button at the bottom</li>
   <li>See detailed <a href="http://youtu.be/712uaZElRRA">video instructions</a> here for scheduling shifts and make-ups:</li>
   <li>Remember, crisis counselors commit to a minimum of one four-hour shift per week for one year</li>
-  <li>Contact <a href="volunteers@crisistextline.org">volunteers@crisistextline.org</a> for any questions about scheduling policies or the scheduling tool</li>
+  <li>Contact <a href="mailto:volunteers@crisistextline.org">volunteers@crisistextline.org</a> for any questions about scheduling policies or the scheduling tool</li>
 </ul>
-<p><strong>Say hello to the supervisors.</strong> Our team of amazing supervisors have your back - they’re on the platform 24/7 to support you during your weekly shifts. They’ll answer your questions and work with in cases of imminent harm or suspected abuse/neglect.</p>
+
+<p><strong>Say hello to the supervisors.</strong> Our team of amazing supervisors have your back - they’re on the platform 24/7 to support you during your weekly shifts. They’ll answer your questions and work with you in cases of imminent harm or suspected abuse/neglect.</p>
+
 <p><strong>Bookmark these links.</strong></p>
 <ul>
   <li>
-    CTL Online: <a href="https://online.crisistextline.org">https://online.crisistextline.org</a>
+    CTL Online: <a href="https://online.crisistextline.org/training">https://online.crisistextline.org</a>
     <ul>
-      <li><a href="https://online.crisistextline.org/schedule/my-schedule">Shift scheduling</a></li>
+      <li><a href="https://online.crisistextline.org/schedule/">Shift scheduling</a></li>
       <li><a href="https://online.crisistextline.org/training">Access training materials</a></li>
     </ul>
   </li>
@@ -27,10 +50,11 @@
     </ul>
   </li>
 </ul>
-<p><strong>Celebrate your accomplishment.</strong><br />
-Access your certificate of completion <a href="https://online.crisistextline.org/user/<?php print $graduate_uid; ?>/certificate">here</a><br />
-Print it out and display it proudly!<br />
-<br />
-</p>
-<p>Onward,</p>
+
+<p><strong>Facebook.</strong></p>
+<p>Join us on our Crisis Text Line Crisis Counselor Facebook group where you can stay up-to-date on spike alerts, platform updates, continuing education and topics related to mental health. This is a great opportunity to connect with all of the Crisis Counselors and to get know one another! Join here: <a href="https://www.facebook.com/groups/252241994946156/">https://www.facebook.com/groups/252241994946156/</a></p>
+
+<p><br />
+Onward,</p>
+
 <p><strong><?php print $trainer_name; ?>, Crisis Counselor Trainer</strong></p>
