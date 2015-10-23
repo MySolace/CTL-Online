f89f35c7f9f801893e1ce49c8953be393965e9e5
Ron Martinez <ron@crisistextline.org>
2015-10-23 16:04:32 -0400

    Fixed a syntax error.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp       = require('gulp'),;
+var gulp       = require('gulp');
 var sass       = require('gulp-ruby-sass');
 var autoprefix = require('gulp-autoprefixer');
 var notify     = require("gulp-notify");
