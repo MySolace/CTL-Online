8318536f63b02bf39cc47ed261e2d325e9d61c80
Ron Martinez <ron@crisistextline.org>
2015-12-19 09:56:36 -0500

    Update graduation email message

--- lib/modules/ctl_graduation/templates/graduation_message.tpl.php
+++ lib/modules/ctl_graduation/templates/graduation_message.tpl.php
@@ -2,28 +2,29 @@
 
 <p>Congratulations on graduating from the Crisis Counselor Training Program! I am so excited to welcome you to the amazing team of crisis counselors who help save lives every day. You're a newly minted crisis counselor, now what?</p>
 
+<p>Good News! You are responsible for 4 hours per week of Crisis Counseling on our platform for 1 year but now you can split it into 2 (two-hour long) shifts! I know, this is different from what you heard in training. We listened to our community's requests and looked at the data. So, this is a brand new policy that all other Crisis Counselors will receive in January (you get first dibs!).</p>
+
+<p><strong>Schedule Your Shift</strong></p>
 <ol>
-  <li>Sign up for your weekly counseling shift!</li>
-  <li>Visit https://online.crisistextline.org/schedule/</li>
-  <li>Click "<strong>Sign up for Shift</strong>"</li>
-  <li>Choose a day of the week</li>
-  <li>Select an available shift (note: all times are in EST)</li>
-  <li>confirm by hitting the "<strong>Sign up for this shift</strong>" button at the bottom</li>
-<ol>
+  <li>Here's a step by step <a href="https://docs.google.com/document/d/1KrUWE5LIeEWk-SGNH6wDRA4ManELUypQhGEonLFfnXo/edit">instructional document</a></li>
+  <li>Here's a step by step <a href="https://www.youtube.com/watch?v=9sdMLF3hHzw&feature=youtu.be">instructional video</a></li>
+  <li><strong>Important:</strong> Make sure you set up your platform account. Search your email inbox for our "Getting Started With Crisis Text Line" message and follow the instructions to setup your account. If you can't find the email, contact <a href="mailto:Kaley@crisistextline.org">Kaley@crisistextline.org</a></li>
+</ol>
 
 <p><strong>Having Trouble?</strong></p>
 <ul>
-  <li>See detailed <a href="http://youtu.be/712uaZElRRA">video instructions</a> here for scheduling shifts and make-ups</li>
-  <li>Remember, crisis counselors commit to the same four-hour shift per week for one year. You may sign up for multiple shifts</li>
-  <li>Contact <a href="mailto:volunteers@crisistextline.org">volunteers@crisistextline.org</a> for any questions about scheduling policies or the scheduling tool</li>
+  <li>See the <a href="https://help.wheniwork.com/hc/en-us/sections/200270007-Employee-Training-Videos-and-Tips">help center</a> from "When I Work"  here for scheduling shifts and make-ups</li>
+  <li>Remember, crisis counselors commit to the same four hours (2 two-hour shifts) every week for one year</li>
+  <li>We recommend you schedule your two 2-hour shifts back to back.
+  Contact <a href="mailto:Volunteers@crisistextline.org">Volunteers@crisistextline.org</a> for questions about scheduling policies. For questions about the scheduling tool, contact When I Work support and their <a href="https://help.wheniwork.com/hc/en-us/sections/200270007-Employee-Training-Videos-and-Tips">help center</a>.</li>
 </ul>
 
-<p>Once you schedule a shift, you'll receive an email from Jen James, Director of Crisis Counselors, who will introduce you to "the Platform,"" which is where you'll go each week for your crisis counseling shift.</p>
+<p>Once you schedule your first shift, you'll receive an email from the Crisis Text Line Training Team, who will introduce you to "the Platform," which is where you'll go each week for your crisis counseling shift.</p>
 
 <p>In the meantime, celebrate! Access your certificate of completion here: <a href="https://online.crisistextline.org/user/<?php print $graduate_uid; ?>/certificate">https://online.crisistextline.org/user/<?php print $graduate_uid; ?>/certificate</a><br />
 Print it out and display it proudly!</p>
 
-<p>Share with others! Share the good news with friends and family and invite them to go through training so they can join you on the platform: <a href="http://crisistextline.org/volunteer">crisistextline.org/volunteer</a>. Volunteering is always more fun with a friend. Our next application deadline is <strong>November 17th</strong>.</p>
+<p>Now tell others! Share the good news with friends and family and invite them to go through training so they can join you on the platform: <a href="http://crisistextline.org/volunteer">crisistextline.org/volunteer</a>. Volunteering is always more fun with a friend. Our next application deadline is <strong>January 14, 2016</strong>.</p>
 
 <ul>
   <li><a href="https://twitter.com/intent/tweet?text=Woo%21%20You%27re%20looking%20at%20a%20newly-minted%20%40CrisisTextLine%20crisis%20counselor%21%20Come%20save%20lives%20with%20me%3A%20http%3A%2F%2Fcrisistextline.org%2Fvolunteer&source=webclient">Twitter</a></li>
@@ -31,6 +32,6 @@ Print it out and display it proudly!</p>
   <li><a href="https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2FpostLogin%3Fsession_rikey%3DwNPHRSO3dcxDUe0v-qmScfRF750Nbez-4y2rllZxykPg17oJcOC3qTWcU5-4nZOU5t_r50pFHsEFXvZln9cpQTyxU8lllA5W1m1%26l%3Dhttps%253A%252F%252Fwww%252Elinkedin%252Ecom%252FshareArticle%253Fmini%253Dtrue%2526source%253Dcrisistextline%25252Eorg%2526title%253DBecome%252Ba%252Bcrisis%252Bcounselor%2526url%253Dcrisistextline%25252Eorg%25252Fvolunteer%26id%3D0%26b%3D41b363a3-d7f1-47f5-831d-2087fe0f8e14%26h%3DwQ5H%26m%3DGET">LinkedIn</a></li>
 </ul>
 
-<p>Congrats!</p>
+<p>See you on the Platform!</p>
 
 <p><strong><?php print $trainer_name; ?>, Crisis Counselor Trainer</strong></p>
